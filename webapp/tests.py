489edb4c4d324ab563714acc37a1bda074f02300
sreenani <sreenani@yahoo.com>
2019-11-09 19:13:28 +0530

    Update tests.py

--- webapp/tests.py
+++ webapp/tests.py
@@ -8,6 +8,7 @@ class AppTestCase(unittest.TestCase):
    def test_root_text(self):
         tester = app.test_client(self)
         response = tester.get('/')
+        print response
         assert 'Hello world!' in response.data
 
 if __name__ == '__main__':
